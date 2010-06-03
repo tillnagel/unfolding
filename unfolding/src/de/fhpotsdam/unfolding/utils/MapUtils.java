72080aebacd256bf4f0268d7e1cf6fc81841e3ff
Till Nagel <github@tillnagel.com>
2010-06-03 12:50:44 +0000

    Added javadoc.

--- unfolding/src/de/fhpotsdam/unfolding/utils/MapUtils.java
+++ unfolding/src/de/fhpotsdam/unfolding/utils/MapUtils.java
@@ -10,6 +10,16 @@ import de.fhpotsdam.unfolding.interactions.MouseHandler;
 
 public class MapUtils {
 
+	/**
+	 * Initializes default events, i.e. all given maps handle mouse and keyboard interactions.
+	 * No cross-listening between maps.
+	 * 
+	 * @param p
+	 *            The PApplet needed for mouse and key user interactions.
+	 * @param maps
+	 *            One or many maps.
+	 * @return The EventDispatcher to use for additional event handling.
+	 */
 	public static EventDispatcher createDefaultEventDispatcher(PApplet p, Map... maps) {
 		EventDispatcher eventDispatcher = new EventDispatcher();
 
@@ -23,8 +33,8 @@ public class MapUtils {
 			eventDispatcher.register(map, PanMapEvent.PAN, map.getId());
 			eventDispatcher.register(map, ZoomMapEvent.ZOOM, map.getId());
 		}
-		
+
 		return eventDispatcher;
 	}
-	
+
 }
