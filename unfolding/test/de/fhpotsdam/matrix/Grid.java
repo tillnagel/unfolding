83d3c968500ad8c482c63a17e41762f51f369d54
Till Nagel <github@tillnagel.com>
2010-06-09 20:54:17 +0000

    Added non-map transformation tests.

---
+++ unfolding/test/de/fhpotsdam/matrix/Grid.java
@@ -0,0 +1,73 @@
+package de.fhpotsdam.matrix;
+
+import processing.core.PApplet;
+import processing.core.PMatrix3D;
+
+public class Grid {
+
+	PApplet p;
+
+	float offsetX;
+	float offsetY;
+
+	float width;
+	float height;
+
+	float centerX;
+	float centerY;
+
+	float angle = 0;
+	float scale = 1;
+
+	PMatrix3D matrix = new PMatrix3D();
+
+	public Grid(PApplet p, float offsetX, float offsetY, float width, float height) {
+		this.p = p;
+
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.width = width;
+		this.height = height;
+
+		this.centerX = width / 2;
+		this.centerY = height / 2;
+	}
+
+	public void draw() {
+		calculateMatrix(centerX, centerY);
+		p.applyMatrix(matrix);
+		p.translate(offsetX, offsetY);
+
+		p.fill(255, 150);
+		p.stroke(0, 150);
+
+		for (float i = 0; i <= width; i += 10) {
+			for (float j = 0; j <= height; j += 10) {
+				p.rect(i, j, 10, 10);
+			}
+		}
+	}
+	
+	protected void calculateMatrix(float x, float y) {
+		// Calculates original position by inverting the current matrix.
+		// As the matrix incorporates that position, it stores every transformation, even though
+		// the matrix is created anew.
+
+		PMatrix3D invMatrix = new PMatrix3D();
+		invMatrix.apply(matrix);
+		invMatrix.invert();
+		float origX = invMatrix.multX(x, y);
+		float origY = invMatrix.multY(x, y);
+
+		p.fill(255, 0, 0, 100);
+		p.ellipse(origX, origY, 15, 15);
+
+		matrix = new PMatrix3D();
+		matrix.translate(x, y);
+		matrix.scale(scale);
+		matrix.rotate(angle);
+		matrix.translate(-origX, -origY);
+	}
+
+
+}
---
+++ unfolding/test/de/fhpotsdam/matrix/GridTest.java
@@ -0,0 +1,47 @@
+package de.fhpotsdam.matrix;
+
+import processing.core.PApplet;
+import codeanticode.glgraphics.GLConstants;
+
+public class GridTest extends PApplet {
+
+	Grid grid;
+
+	public void setup() {
+		size(800, 600, GLConstants.GLGRAPHICS);
+		smooth();
+
+		grid = new Grid(this, 50, 50, 300, 200);
+	}
+
+	public void draw() {
+		background(240);
+
+		if (mousePressed) {
+			if (mouseButton == LEFT) {
+				grid.angle += 0.01f;
+			} else {
+				grid.angle -= 0.01f;
+			}
+		}
+		if (keyPressed) {
+			if (key == '+') {
+				grid.scale += 0.01f;
+			} else if (key == '-') {
+				grid.scale -= 0.01f;
+			}
+
+		}
+
+		grid.draw();
+	}
+	
+	public void mouseMoved() {
+		grid.centerX = mouseX;
+		grid.centerY = mouseY;
+	}
+
+	public void mouseDragged() {
+		mouseMoved();
+	}
+}
