2ef6cc52f7027b8408484aedfc1489cc0d2235ec
Till Nagel <github@tillnagel.com>
2010-07-02 12:20:15 +0000

    Added keyboard test interactions, and debug messages. Added approach with multiple matrices to enable correct offset handling.

--- unfolding/test/de/fhpotsdam/transmatrix/MultiTransObjectApp.java
+++ unfolding/test/de/fhpotsdam/transmatrix/MultiTransObjectApp.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import processing.core.PApplet;
+import processing.core.PMatrix3D;
 import TUIO.TuioClient;
 import TUIO.TuioCursor;
 import TUIO.TuioListener;
@@ -14,8 +15,10 @@ import TUIO.TuioTime;
 import codeanticode.glgraphics.GLConstants;
 
 /**
- * Fully working test app to freely transform an object (Grid). Rotate, Scale (and implicit
- * transform by the former two) work.
+ * Fully working test app to freely transform multiple independent objects. Rotate, Scale (and
+ * implicit transform by the former two) work.
+ * 
+ * Not working: Transform, i.e. changing the offset.
  * 
  */
 public class MultiTransObjectApp extends PApplet implements TuioListener {
@@ -27,8 +30,8 @@ public class MultiTransObjectApp extends PApplet implements TuioListener {
 	TuioClient tuioClient;
 
 	public void setup() {
-		size(1024, 768, GLConstants.GLGRAPHICS);
-		// size(800, 400, GLConstants.GLGRAPHICS);
+		// size(1024, 768, GLConstants.GLGRAPHICS);
+		size(800, 400, GLConstants.GLGRAPHICS);
 		smooth();
 		textFont(loadFont("Miso-Light-12.vlw"), 12);
 
@@ -36,8 +39,8 @@ public class MultiTransObjectApp extends PApplet implements TuioListener {
 		tuioClient.addTuioListener(this);
 		tuioClient.connect();
 
-		transObjects.add(new TuioTransformableObject(this, 100, 100, 300, 300));
-		transObjects.add(new TuioTransformableObject(this, 500, 200, 300, 300));
+		transObjects.add(new TuioTransformableObject(this, 0, 0, 300, 300));
+		// transObjects.add(new TuioTransformableObject(this, 500, 200, 300, 300));
 	}
 
 	public void draw() {
@@ -50,15 +53,78 @@ public class MultiTransObjectApp extends PApplet implements TuioListener {
 		// Display debug information
 		drawCursors();
 		fill(0);
-		text("fps: " + nfs(frameRate, 3, 3), 20, 20);
+		text("fps: " + nfs(frameRate, 2, 3), 20, 20);
+
 	}
 
 	public void keyPressed() {
-		transObjects.get(0).setOffset(50, 50);
+
+		TransformableObject to = transObjects.get(0);
+
+		if (key == 'r') {
+			println("rotate cw");
+			to.rotate(radians(45));
+		}
+		if (key == 'l') {
+			println("rotate ccw");
+			to.rotate(-radians(45));
+		}
+		if (key == 'm') {
+			println("using mouse-pos as center: " + mouseX + "," + mouseY);
+			to.centerX = mouseX;
+			to.centerY = mouseY;
+		}
+
+		if (key == 'c') {
+			float[] check = to.getTransformedPosition(300, 300, false);
+			println("using transPos for center: " + check[0] + "," + check[1]);
+			to.setCenter(check[0], check[1]);
+		}
+
+		if (key == CODED) {
+			if (keyCode == LEFT) {
+				println("left");
+				to.addOffset(-100, 0);
+			}
+			if (keyCode == RIGHT) {
+				println("right");
+				to.addOffset(100, 0);
+			}
+		}
+
+		if (key == 's') {
+			to.scale(0.95f);
+		}
+		if (key == 'S') {
+			to.scale(1.05f);
+		}
+
+		if (key == 'i') {
+			float[] check = to.getTransformedPosition(0, 0, false);
+			println("0,0: " + check[0] + "," + check[1]);
+			println("ctr: " + to.centerX + "," + to.centerY);
+		}
+	}
+
+	public String getMatrix3DAsString(PMatrix3D m) {
+		return nfs(m.m00, 2, 2) + "," + nfs(m.m01, 2, 2) + "," + nfs(m.m02, 2, 2) + ","
+				+ nfs(m.m03, 2, 2) + "\n" + nfs(m.m10, 2, 2) + "," + nfs(m.m11, 2, 2) + ","
+				+ nfs(m.m12, 2, 2) + "," + nfs(m.m13, 2, 2) + "\n" + nfs(m.m20, 2, 2) + ","
+				+ nfs(m.m21, 2, 2) + "," + nfs(m.m22, 2, 2) + "," + nfs(m.m33, 2, 2) + "\n"
+				+ nfs(m.m30, 2, 2) + "," + nfs(m.m31, 2, 2) + "," + nfs(m.m32, 2, 2) + ","
+				+ nfs(m.m33, 2, 2) + "\n";
+	}
+
+	public void mouseDragged() {
+		float dx = mouseX - pmouseX;
+		float dy = mouseY - pmouseY;
+		transObjects.get(0).addOffset(dx, dy);
+
+		// transObjects.get(0).setOffset(mouseX, mouseY);
 	}
 
 	public void addTuioCursor(TuioCursor tcur) {
-		// Hit test for all objects, ordered by creation
+		// Hit test for all objects: first gets the hit, ordered by creation.
 		// TODO Order by z-index, updated by last activation/usage
 		for (TuioTransformableObject ttObj : transObjects) {
 			if (ttObj.isHit(tcur.getScreenX(width), tcur.getScreenY(height))) {
@@ -70,13 +136,10 @@ public class MultiTransObjectApp extends PApplet implements TuioListener {
 
 	public void removeTuioCursor(TuioCursor tcur) {
 		for (TuioTransformableObject ttObj : transObjects) {
-			// Pass trough remove to all objects, to allow fingerUp also out of boundaries,
+			// Pass trough remove-event to all objects, to allow fingerUp also out of boundaries,
 			// as objects decide themselves (via cursor-id) whether cursor belongs to it.
-			
-			// if (ttObj.isHit(tcur.getScreenX(width), tcur.getScreenY(height))) {
+
 			ttObj.removeTuioCursor(tcur);
-			// break;
-			// }
 		}
 	}
 
--- unfolding/test/de/fhpotsdam/transmatrix/TransformableObject.java
+++ unfolding/test/de/fhpotsdam/transmatrix/TransformableObject.java
@@ -39,12 +39,14 @@ public class TransformableObject {
 		this.centerY = height / 2;
 
 		this.color = p.color(255, 150);
+
+		calculateMatrix();
 	}
 
 	public void draw() {
 		p.pushMatrix();
+//		p.translate(offsetX, offsetY);
 		p.applyMatrix(matrix);
-		p.translate(offsetX, offsetY);
 
 		internalDraw();
 
@@ -55,11 +57,19 @@ public class TransformableObject {
 		p.fill(color);
 		p.stroke(0, 150);
 
-		for (float i = 0; i <= width; i += 10) {
-			for (float j = 0; j <= height; j += 10) {
+		for (float i = 0; i <= width - 10; i += 10) {
+			for (float j = 0; j <= height - 10; j += 10) {
 				p.rect(i, j, 10, 10);
 			}
 		}
+
+		p.fill(255);
+		p.rect(0, 0, 50, 20);
+		p.fill(0);
+		p.text(">>> ABC >>>", 0, 13);
+		
+		p.fill(255, 0, 0, 100);
+		p.ellipse(centerX, centerY, 10, 10);
 	}
 
 	public void setColor(int color) {
@@ -68,14 +78,16 @@ public class TransformableObject {
 
 	public boolean isHit(int checkX, int checkY) {
 		float[] check = getTransformedPosition(checkX, checkY, true);
-		return (check[0] > offsetX && check[0] < offsetX + width && check[1] > offsetY && check[1] < offsetY
-				+ height);
+//		return (check[0] > offsetX && check[0] < offsetX + width && check[1] > offsetY && check[1] < offsetY
+//				+ height);
+		 return (check[0] > 0 && check[0] < 0 + width && check[1] > 0 && check[1] < 0 + height);
 	}
 
 	public float[] getTransformedPosition(float x, float y, boolean inverse) {
 		float[] preXY = new float[3];
 		PMatrix3D m = new PMatrix3D();
 		m.apply(matrix);
+		//m.translate(-offsetX, -offsetY);
 		if (inverse) {
 			m.invert();
 		}
@@ -92,15 +104,17 @@ public class TransformableObject {
 		this.scale *= scale;
 		calculateMatrix();
 	}
-	
-	public void translate(float dx, float dy) {
+
+	public void addOffset(float dx, float dy) {
 		this.offsetX += dx;
 		this.offsetY += dy;
+		calculateMatrix();
 	}
 
 	public void setOffset(float x, float y) {
 		this.offsetX = x;
 		this.offsetY = y;
+		calculateMatrix();
 	}
 
 	public void setCenter(float centerX, float centerY) {
@@ -109,21 +123,69 @@ public class TransformableObject {
 	}
 
 	protected void calculateMatrix() {
-		// Calculates original position by inverting the current matrix.
-		// As the matrix incorporates that position, it stores every transformation, even though
-		// the matrix is created anew.
+		//calculateMatrixWithNewMatrix();
+		
+		calculateMatrixSingleMatrix();
+	}
+	
+	boolean first = true;
+	float oldOffsetX;
+	float oldOffsetY;
+	
+	// 
+	protected void calculateMatrixSingleMatrix() {
+		if (first) {
+			first = false;
+		} else {
+			matrix.translate(-oldOffsetX, -oldOffsetY);
+		}
 
 		PMatrix3D invMatrix = new PMatrix3D();
 		invMatrix.apply(matrix);
 		invMatrix.invert();
-		float originalX = invMatrix.multX(centerX, centerY);
-		float originalY = invMatrix.multY(centerX, centerY);
 
+		float originalCenterX = invMatrix.multX(centerX, centerY);
+		float originalCenterY = invMatrix.multY(centerX, centerY);
+		
 		matrix = new PMatrix3D();
 		matrix.translate(centerX, centerY);
 		matrix.scale(scale);
 		matrix.rotate(angle);
-		matrix.translate(-originalX, -originalY);
+		matrix.translate(-originalCenterX, -originalCenterY);
+		matrix.translate(offsetX, offsetY);
+		
+		oldOffsetX = offsetX;
+		oldOffsetY = offsetY;
+	}
+
+	PMatrix3D matrix2 = new PMatrix3D();
+	
+	protected void calculateMatrixWithNewMatrix() {
+		if (first) {
+			first = false;
+		} else {
+			matrix.translate(-oldOffsetX, -oldOffsetY);
+		}
+
+		PMatrix3D invMatrix = new PMatrix3D();
+		invMatrix.apply(matrix2);
+		invMatrix.invert();
+
+		float originalCenterX = invMatrix.multX(centerX, centerY);
+		float originalCenterY = invMatrix.multY(centerX, centerY);
+		
+		matrix2 = new PMatrix3D();
+		matrix2.translate(centerX, centerY);
+		matrix2.scale(scale);
+		matrix2.rotate(angle);
+		matrix2.translate(-originalCenterX, -originalCenterY);
+		
+		matrix = new PMatrix3D();
+		matrix.translate(offsetX, offsetY);
+		matrix.apply(matrix2);
+		
+		oldOffsetX = offsetX;
+		oldOffsetY = offsetY;
 	}
 
 	protected float getAngleBetween(float x1, float y1, float x2, float y2) {
