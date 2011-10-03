d8fe1f377b68f151a4b029548522eff56d511ac9
Till Nagel <github@tillnagel.com>
2011-10-03 19:04:58 +0000

    Extended rotation example. Now both inner and outer rotations can be done to compare the two methods.

--- unfolding/examples/de/fhpotsdam/unfolding/examples/RotatableMapApp.java
+++ unfolding/examples/de/fhpotsdam/unfolding/examples/RotatableMapApp.java
@@ -8,6 +8,14 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.utils.DebugDisplay;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
+/**
+ * Application to compare the two different map rotation styles. Inner-rotate transforms the map
+ * itself, and works with all renderers. Outer-rotate the map container, and works with GLGraphics,
+ * only.
+ * 
+ * Use r and l to inner rotate clock-wise (right), and counter-clock-wise (left), and R and L to
+ * outer rotate.
+ */
 public class RotatableMapApp extends PApplet {
 
 	Map map;
@@ -16,11 +24,18 @@ public class RotatableMapApp extends PApplet {
 	PVector rotateCenter = new PVector(350, 250);
 	Location location = new Location(51.50939f, -0.11820f);
 
-	public void setup() {
-		size(800, 600, GLConstants.GLGRAPHICS);
+	boolean fullRotatable = true;
 
-		map = new Map(this, "map1", 0, 0, 700, 500);
+	public void setup() {
+		if (fullRotatable) {
+			size(800, 600, GLConstants.GLGRAPHICS);
+		} else {
+			size(800, 600);
+		}
+
+		map = new Map(this, "map1", 50, 50, 700, 500);
 		map.setTweening(false);
+		map.zoomToLevel(3);
 		MapUtils.createDefaultEventDispatcher(this, map);
 
 		textFont(loadFont("Miso-Light-12.vlw"), 20);
@@ -44,10 +59,18 @@ public class RotatableMapApp extends PApplet {
 		rotateCenter = new PVector(mouseX, mouseY);
 		map.mapDisplay.setTransformationCenter(rotateCenter);
 
+		// Inner rotate (i.e. map) works with both, P2D and GLGraphics
 		if (key == 'r') {
 			map.rotate(-PI / 8);
 		} else if (key == 'l') {
 			map.rotate(PI / 8);
+		}
+
+		// Outer rotate (i.e. map container) only works with GLGraphics offscreen buffer
+		if (key == 'R') {
+			map.outerRotate(-PI / 8);
+		} else if (key == 'L') {
+			map.outerRotate(PI / 8);
 		}
 	}
 
