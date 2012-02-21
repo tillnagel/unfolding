59e71953311f58f8dde665b47c210dc7c905aef1
Till Nagel <github@tillnagel.com>
2012-02-21 02:02:17 +0000

    Added interactivity to great circle route example.

--- unfolding/examples/de/fhpotsdam/unfolding/examples/GreatCircleConnectionApp.java
+++ unfolding/examples/de/fhpotsdam/unfolding/examples/GreatCircleConnectionApp.java
@@ -7,12 +7,21 @@ import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.utils.GeoUtils;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
+/**
+ * Shows a great circle connection between two locations. A great circle connection is an approximation of the shortest
+ * route between two places on earth.
+ * 
+ * Mouse move and SHIFT or CTRL to move the locations.
+ */
 public class GreatCircleConnectionApp extends PApplet {
+
+	final Location singaporeLocation = new Location(1, 100);
+	final Location berlinLocation = new Location(54, 13.5f);
 
 	Map map;
 
-	Location singaporeLocation = new Location(1, 100);
-	Location berlinLocation = new Location(54, 13.5f);
+	Location sourceLocation = berlinLocation;
+	Location targetLocation = singaporeLocation;
 
 	public void setup() {
 		size(800, 600, GLConstants.GLGRAPHICS);
@@ -26,26 +35,38 @@ public class GreatCircleConnectionApp extends PApplet {
 		map.draw();
 
 		fill(0, 0, 255);
-		float[] sourcePos = map.getScreenPositionFromLocation(singaporeLocation);
+		float[] sourcePos = map.getScreenPositionFromLocation(targetLocation);
 		ellipse(sourcePos[0], sourcePos[1], 10, 10);
 
 		fill(255, 0, 0);
-		float[] targetPos = map.getScreenPositionFromLocation(berlinLocation);
+		float[] targetPos = map.getScreenPositionFromLocation(sourceLocation);
 		ellipse(targetPos[0], targetPos[1], 10, 10);
 
-		double bearing = GeoUtils.getAngleBetween(singaporeLocation, berlinLocation);
-		double dist = GeoUtils.getDistance(singaporeLocation, berlinLocation);
+		double bearing = GeoUtils.getAngleBetween(targetLocation, sourceLocation);
+		double dist = GeoUtils.getDistance(targetLocation, sourceLocation);
 
 		noFill();
 		strokeWeight(2);
 		stroke(0, 100);
 		beginShape();
 		for (float d = 0; d < dist; d += 100) {
-			Location tweenLocation = GeoUtils.getDestinationLocation(singaporeLocation, degrees((float) bearing),
+			Location tweenLocation = GeoUtils.getDestinationLocation(targetLocation, degrees((float) bearing),
 					(float) d);
 			float[] tweenPos = map.getScreenPositionFromLocation(tweenLocation);
 			vertex(tweenPos[0], tweenPos[1]);
 		}
 		endShape();
 	}
+
+	public void mouseMoved() {
+		if (keyPressed && key == CODED) {
+			if (keyCode == SHIFT) {
+				targetLocation = map.getLocationFromScreenPosition(mouseX, mouseY);
+			}
+			if (keyCode == CONTROL) {
+				sourceLocation = map.getLocationFromScreenPosition(mouseX, mouseY);
+			}
+		}
+	}
+
 }
