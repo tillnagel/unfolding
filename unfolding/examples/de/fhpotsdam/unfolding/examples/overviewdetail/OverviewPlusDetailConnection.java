ff0fa60802d57b16d5b120446507da46dc1950c5
Till Nagel <github@tillnagel.com>
2012-02-28 08:20:48 +0000

    Added OverviewPlusDetailConnection

---
+++ unfolding/examples/de/fhpotsdam/unfolding/examples/overviewdetail/OverviewPlusDetailConnection.java
@@ -0,0 +1,17 @@
+package de.fhpotsdam.unfolding.examples.overviewdetail;
+
+public interface OverviewPlusDetailConnection {
+	
+	public void setPadding(float padding);
+
+	public void setDetailPosition(float x, float y);
+	
+	public void setDetailSize(float width, float height);
+	
+	public void setOverviewPosition(float x, float y);
+	
+	public void setOverviewSize(float width, float height);
+
+	public void draw();
+	
+}
