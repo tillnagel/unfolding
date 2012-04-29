5b39268fe86f6abdb76632000d54e491b2bd0cb2
Gunnar Friedrich <Gunnar@Gunnars-MacBook-Pro.local>
2012-04-29 17:34:27 +0200

    rename Compass UI

--- examples/de/fhpotsdam/unfolding/examples/ui/SimpleCompassApp.java
+++ examples/de/fhpotsdam/unfolding/examples/ui/SimpleCompassApp.java
@@ -10,14 +10,14 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 public class SimpleCompassApp extends PApplet {
 
 	Map map;
-	Compass compass;
+	CompassUI compass;
 
 	public void setup() {
 		size(800, 600, GLConstants.GLGRAPHICS);
 
 		map = new Map(this);
 		map.zoomAndPanTo(new Location(52.5f, 13.4f), 10);
-		compass = new Compass(this,map);
+		compass = new CompassUI(this,map);
 
 		MapUtils.createDefaultEventDispatcher(this, map);
 	}
@@ -27,6 +27,11 @@ public class SimpleCompassApp extends PApplet {
 		map.draw();
 		compass.draw();
 	}
+	
+	public void keyPressed() {
+		if (key == 'a') map.rotate(0.1f);
+		if (key == 'd') map.rotate(-0.1f);
+		}
 
 	public static void main(String[] args) {
 		PApplet.main(new String[] { "de.fhpotsdam.unfolding.examples.SimpleCompassApp" });
