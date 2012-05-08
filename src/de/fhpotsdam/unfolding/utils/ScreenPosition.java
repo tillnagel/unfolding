361c860dba5d4f7ae82a21274992d079fb20b047
Stefan Otte <stefan.otte@gmail.com>
2012-05-08 14:05:39 +0200

    Class that represents ScreenPosition
    This should replace the omni present float[] xy.

---
+++ src/de/fhpotsdam/unfolding/utils/ScreenPosition.java
@@ -0,0 +1,13 @@
+package de.fhpotsdam.unfolding.utils;
+
+
+public class ScreenPosition {
+	
+	public int x;
+	public int y;
+	
+	public ScreenPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
