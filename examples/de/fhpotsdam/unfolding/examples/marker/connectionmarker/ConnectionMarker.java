ae5badd198a99f1deb7e8fe4cddaf15af5dbc960
Felix Lange <fl@spta.de>
2012-05-26 13:56:05 +0200

    connectionmarker as multimarker
    neighborMarkerApp broken

--- examples/de/fhpotsdam/unfolding/examples/marker/connectionmarker/ConnectionMarker.java
+++ examples/de/fhpotsdam/unfolding/examples/marker/connectionmarker/ConnectionMarker.java
@@ -5,31 +5,32 @@ import processing.core.PVector;
 import de.fhpotsdam.unfolding.Map;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.AbstractMarker;
+import de.fhpotsdam.unfolding.marker.AbstractMultiMarker;
 import de.fhpotsdam.unfolding.marker.Marker;
 
-public class ConnectionMarker extends AbstractMarker {
+public class ConnectionMarker extends AbstractMultiMarker {
 
-	public Marker fromMarker;
-	public Marker toMarker;
+//	public Marker fromMarker;
+//	public Marker toMarker;
 
 	public ConnectionMarker(Marker fromMarker, Marker toMarker) {
-		this.fromMarker = fromMarker;
-		this.toMarker = toMarker;
+		addLocation(fromMarker.getLocation());
+		addLocation(toMarker.getLocation());
 	}
 
 	@Override
 	public void drawOuter(Map map) {
 		PGraphics pg = map.mapDisplay.getPG();
-		float[] xy1 = map.mapDisplay.getScreenPositionFromLocation(fromMarker.getLocation());
+		float[] xy1 = map.mapDisplay.getScreenPositionFromLocation(getLocation(0));
 		PVector v1 = new PVector(xy1[0], xy1[1]);
-		float[] xy2 = map.mapDisplay.getScreenPositionFromLocation(toMarker.getLocation());
+		float[] xy2 = map.mapDisplay.getScreenPositionFromLocation(getLocation(1));
 		PVector v2 = new PVector(xy2[0], xy2[1]);
 		pg.line(v1.x, v1.y, v2.x, v2.y);
 	}
 
 	@Override
 	public Location getLocation() {
-		return fromMarker.getLocation();
+		return getLocation(0);
 	}
 
 	@Override
--- src/de/fhpotsdam/unfolding/marker/AbstractMultiMarker.java
+++ src/de/fhpotsdam/unfolding/marker/AbstractMultiMarker.java
@@ -1,14 +1,36 @@
 package de.fhpotsdam.unfolding.marker;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.fhpotsdam.unfolding.geo.Location;
 
 public abstract class AbstractMultiMarker extends AbstractMarker{
+	
 	protected List<Location> locations;
+
+	public AbstractMultiMarker() {
+		locations = new ArrayList<Location>();
+	}
 	
 	public void setLocations(List<Location> locations){
 		this.locations = locations;
+	}
+	
+	public void addLocation(Location location){
+		locations.add(location);
+	}
+	
+	public Location getLocation(int index){
+		return locations.get(index);
+	}
+	
+	public void removeLocation(Location location){
+		locations.remove(location);
+	}
+	
+	public void removeLocation(int index){
+		locations.remove(index);
 	}
 	
 	//REVISIT default behavior for getLocation()?
