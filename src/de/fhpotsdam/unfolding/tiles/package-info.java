3c15d8e7971b91154b3e7f8d72eaf909d5aaa6f1
Stefan Otte <stefan.otte@gmail.com>
2012-06-12 12:36:16 +0200

    Document tiles package.

--- src/de/fhpotsdam/unfolding/tiles/package-info.java
+++ src/de/fhpotsdam/unfolding/tiles/package-info.java
@@ -1,5 +1,8 @@
 /**
-TODO: Document me!
+ * Handling all tile related stuff.
+ * 
+ * Tiles are the actual visualization of the map. Different tiles can cab be
+ * used.
  */
 
 package de.fhpotsdam.unfolding.tiles;
