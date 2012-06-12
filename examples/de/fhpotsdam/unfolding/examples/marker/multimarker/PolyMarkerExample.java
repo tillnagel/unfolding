22a38f6c14426dfc62b5488d9d79c824f06ed9eb
Felix Lange <fl@spta.de>
2012-06-12 12:33:21 +0200

    just comments

--- examples/de/fhpotsdam/unfolding/examples/marker/multimarker/PolyMarkerExample.java
+++ examples/de/fhpotsdam/unfolding/examples/marker/multimarker/PolyMarkerExample.java
@@ -1,9 +1,13 @@
 package de.fhpotsdam.unfolding.examples.marker.multimarker;
 
+import javax.swing.text.html.HTMLEditorKit.Parser;
+
 import processing.core.PApplet;
 import codeanticode.glgraphics.GLConstants;
 import de.fhpotsdam.unfolding.Map;
 import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.MarkerManager;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 @SuppressWarnings("serial")
@@ -24,7 +28,23 @@ public class PolyMarkerExample extends PApplet {
 		area.addLocations(new Location(51.5f, 0.0f));
 		area.addLocations(new Location(48.f, 5f));
 		area.addLocations(new Location(52.5f, 13.4f+0.5f));
+		
 		map.addMarkers(area);
+		
+//		MarkerManager<Marker> fluesse;
+//		MarkerManager<Marker> strassen;
+//		
+//		map.addMarkerManager(fluesse);
+//		map.addMarkerManager(strassen);
+		
+//		map.moveLayer(flusse,-1);
+		
+//		flugParser.pasrseTo(mm,color);
+//		mm.setColor();
+		
+		//mouse
+//		fluesse.enableDrawing();
+//		fluesse.disableDrawing();
 	}
 	
 	public void draw(){
