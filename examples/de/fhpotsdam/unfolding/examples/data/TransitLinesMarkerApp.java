803310a584949901688180d7569c077005124a9b
Till Nagel <tillnagel@Tills-MacBook-Pro.local>
2012-06-19 11:45:15 +0200

    Removed task for felix.

--- examples/de/fhpotsdam/unfolding/examples/data/TransitLinesMarkerApp.java
+++ examples/de/fhpotsdam/unfolding/examples/data/TransitLinesMarkerApp.java
@@ -37,7 +37,6 @@ public class TransitLinesMarkerApp extends PApplet {
 		List<Feature> transitLines = GeoJSONReader.loadData(this, "MBTARapidTransitLines.json");
 		List<Marker> transitMarkers = MapUtils.createSimpleMarkers(transitLines);
 		
-		// TODO @felix: Why connect manager to map twice?
 		transitLinesManager = new MarkerManager<Marker>();
 		transitLinesManager.setMarkers(transitMarkers);
 		map.addMarkerManager(transitLinesManager);
