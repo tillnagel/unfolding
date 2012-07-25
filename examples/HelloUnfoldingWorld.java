0208d8b235373ebf44c3c835bc5ea7f18049b1b6
Till Nagel <tillnagel@Tills-MacBook-Pro.local>
2012-07-25 11:29:27 +0200

    Fixed UnfoldingAppTemplate export/build script.
    Updated README to describe how to use native libs (and moved to main
    dir).
    Fixed some minor things in build.xml.

--- examples/HelloUnfoldingWorld.java
+++ examples/HelloUnfoldingWorld.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 import codeanticode.glgraphics.GLConstants;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 
 /**
@@ -16,7 +17,7 @@ public class HelloUnfoldingWorld extends PApplet {
 	public void setup() {
 		size(800, 600, GLConstants.GLGRAPHICS);
 
-		map = new UnfoldingMap(this);
+		map = new UnfoldingMap(this, new OpenStreetMap.OpenStreetMapProvider());
 		map.zoomAndPanTo(new Location(52.5f, 13.4f), 10);
 
 		MapUtils.createDefaultEventDispatcher(this, map);
