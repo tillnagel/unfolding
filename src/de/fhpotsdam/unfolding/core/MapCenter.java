64f6527a7669f30bfd768ddfa6a66540ad4a88b0
Stefan Otte <stefan.otte@gmail.com>
2012-08-19 11:07:28 +0200

    Fix indentation.

--- src/de/fhpotsdam/unfolding/core/MapCenter.java
+++ src/de/fhpotsdam/unfolding/core/MapCenter.java
@@ -4,12 +4,11 @@ import processing.core.PVector;
 
 public class MapCenter {
 
-  public Coordinate coordinate;
-  public PVector point; 
+	public Coordinate coordinate;
+	public PVector point;
 
-  public MapCenter(Coordinate coordinate, PVector point) {
-    this.coordinate = coordinate;
-    this.point = point;
-  }
-
+	public MapCenter(Coordinate coordinate, PVector point) {
+		this.coordinate = coordinate;
+		this.point = point;
+	}
 }
