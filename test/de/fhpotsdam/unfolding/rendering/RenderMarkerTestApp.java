d1b2a885e8914e6e774297f5ad49aece0a4c2ba7
Till Nagel <github@tillnagel.com>
2012-08-31 12:48:28 +0200

    Added test app for issue #45

---
+++ test/de/fhpotsdam/unfolding/rendering/RenderMarkerTestApp.java
@@ -0,0 +1,50 @@
+package de.fhpotsdam.unfolding.rendering;
+
+import processing.core.PApplet;
+import codeanticode.glgraphics.GLConstants;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.utils.ScreenPosition;
+
+/**
+*/
+public class RenderMarkerTestApp extends PApplet {
+
+	UnfoldingMap map;
+	SimplePointMarker berlinMarker;
+
+	public void setup() {
+		size(800, 600, GLConstants.GLGRAPHICS);
+		//size(800, 600);
+		//smooth();
+
+		map = new UnfoldingMap(this);
+		map.zoomToLevel(3);
+		map.panTo(new Location(40f, -98f));
+		MapUtils.createDefaultEventDispatcher(this, map);
+
+		Location berlinLocation = new Location(40f, -98f);
+		berlinMarker = new TestMarker(berlinLocation);
+		berlinMarker.setColor(color(255, 0, 0, 100));
+		berlinMarker.setStrokeColor(color(255, 0, 0));
+		berlinMarker.setStrokeWeight(2);
+		map.addMarkers(berlinMarker);
+	}
+
+	public void draw() {
+		background(240);
+		map.draw();
+
+		ScreenPosition pos = berlinMarker.getScreenPosition(map);
+		fill(255, 0, 0, 100);
+		stroke(255, 0, 0);
+		strokeWeight(2);
+		ellipse(pos.x, pos.y + 30, 20, 20);
+		println("o:" + pos.x + ", " + pos.y);
+		fill(0);
+		text("Xylophon", pos.x + 20, pos.y + 30);
+	}
+
+}
---
+++ test/de/fhpotsdam/unfolding/rendering/TestMarker.java
@@ -0,0 +1,36 @@
+package de.fhpotsdam.unfolding.rendering;
+
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+
+public class TestMarker extends SimplePointMarker {
+
+	public TestMarker(Location location) {
+		super(location);
+		radius = 20;
+	}
+
+	@Override
+	public void draw(PGraphics pg, float x, float y) {
+		
+		int xi = Math.round(x);
+		int yi = Math.round(y);
+
+		PApplet.println("m: " + x + ", " + y);
+		PApplet.println("m: " + xi + ", " + yi);
+
+		pg.pushStyle();
+		pg.strokeWeight(strokeWeight);
+		pg.fill(color);
+		pg.stroke(strokeColor);
+		pg.ellipse(xi, yi, radius, radius);
+
+		pg.fill(0);
+		pg.text("Xylophon", xi + 20, yi);
+
+		pg.popStyle();
+	}
+
+}
