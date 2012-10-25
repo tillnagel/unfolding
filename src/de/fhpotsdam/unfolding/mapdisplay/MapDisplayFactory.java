2d002301c22ad2ded2dbf103bec85e8e4ce538c4
Till Nagel <github@tillnagel.com>
2012-10-25 16:40:16 +0200

    Switched from log4j to println
    Related to #34

--- src/de/fhpotsdam/unfolding/mapdisplay/MapDisplayFactory.java
+++ src/de/fhpotsdam/unfolding/mapdisplay/MapDisplayFactory.java
@@ -1,7 +1,5 @@
 package de.fhpotsdam.unfolding.mapdisplay;
 
-import org.apache.log4j.Logger;
-
 import processing.core.PApplet;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
@@ -18,8 +16,6 @@ public class MapDisplayFactory {
 
 	public static final String OSM_API_KEY = "607e6483654b5c47b9056791d607ab74";
 	public static final int OSM_STYLE_ID = 65678; // test: 69960; // original: 998
-
-	public static Logger log = Logger.getLogger(MapDisplayFactory.class);
 
 	public static AbstractMapDisplay getMapDisplay(PApplet p, String id, float x, float y, float width, float height,
 			AbstractMapProvider provider, UnfoldingMap map) {
@@ -40,10 +36,11 @@ public class MapDisplayFactory {
 				Class glGraphicsClass = Class.forName(GLGRAPHICS_CLASSNAME);
 				if (glGraphicsClass.isInstance(p.g)) {
 					if (useDistortion) {
-						log.debug("Using DistortedGLGraphicsMapDisplay for '" + id + "'");
+						//log.debug("Using DistortedGLGraphicsMapDisplay for '" + id + "'");
 						mapDisplay = new DistortedGLGraphicsMapDisplay(p, provider, x, y, width, height);
 					} else {
-						log.debug("Using GLGraphicsMapDisplay for '" + id + "'");
+						//log.debug("Using GLGraphicsMapDisplay for '" + id + "'");
+						PApplet.println("Using GLGraphicsMapDisplay.");
 						// TODO @chris: Why always use MaskedGLGraphicsMD?
 						// mapDisplay = new MaskedGLGraphicsMapDisplay(p, provider, x, y, width, height);
 						mapDisplay = new GLGraphicsMapDisplay(p, provider, x, y, width, height);
@@ -57,14 +54,16 @@ public class MapDisplayFactory {
 				try {
 					Class openGLClass = Class.forName(OPEN_GL_CLASSNAME);
 					if (openGLClass.isInstance(p.g)) {
-						log.warn("No OpenGL mapDisplay available. Use GLGraphics or P3D. '" + id + "'");
+						//log.warn("No OpenGL mapDisplay available. Use GLGraphics or P3D. '" + id + "'");
+						PApplet.println("No OpenGL mapDisplay available. Use GLGraphics or P2D.");
 					}
 				} catch (ClassNotFoundException e) {
-					// OpenGL not found, was for informational purposes anyway. 
+					// OpenGL not found, was for informational purposes anyway.
 				}
 
-				log.debug("Using MaskedPGraphicsMapDisplay for '" + id + "'");
-				log.warn("no rotation possible (without OpenGL)");
+				//log.debug("Using MaskedPGraphicsMapDisplay for '" + id + "'");
+				//log.warn("no rotation possible (without OpenGL)");
+				PApplet.println("Using MaskedPGraphicsMapDisplay. No rotation possible (w/o GLGraphics)");
 				mapDisplay = new MaskedPGraphicsMapDisplay(p, provider, x, y, width, height);
 			}
 
