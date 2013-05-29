a9217d223bf39456d3e2d87e430becfaca4e3de7
Till Nagel <github@tillnagel.com>
2013-05-29 16:39:11 +0200

    Removed BSD license after feedback from @mourner (original author)

--- src/de/fhpotsdam/unfolding/utils/GeneralizationUtils.java
+++ src/de/fhpotsdam/unfolding/utils/GeneralizationUtils.java
@@ -79,27 +79,6 @@ public class GeneralizationUtils {
 	 * 
 	 * <p>
 	 * Based on Simplify.js by Vladimir Agafonkin. Ported to Java and modified for Unfolding by Till Nagel.
-	 * 
-	 * <p>
-	 * Copyright (c) 2012, Vladimir Agafonkin. All rights reserved.
-	 * 
-	 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-	 * following conditions are met:
-	 * 
-	 * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
-	 * following disclaimer.
-	 * 
-	 * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-	 * following disclaimer in the documentation and/or other materials provided with the distribution.
-	 * 
-	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-	 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-	 * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-	 * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-	 * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-	 * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-	 * </p>
 	 * </p>
 	 * 
 	 * @param points
@@ -118,13 +97,10 @@ public class GeneralizationUtils {
 			points = simplifyRadialDistance(points, sqTolerance);
 		}
 
-		// int numPoints = points.size();
 		points = simplifyDouglasPeucker(points, sqTolerance);
-		// int numSimplifiedPoints = points.size();
-		// PApplet.println("Reduced " + numPoints + " to " + numSimplifiedPoints + " points.");
 
 		return points;
-	};
+	}
 
 	/**
 	 * Square distance between 2 points.
