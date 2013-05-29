ff44c45083da87665f4d496ec00d3ac87d55123a
Till Nagel <github@tillnagel.com>
2013-05-29 16:17:40 +0200

    Added moving average method to GeneralizationUtils.

--- src/de/fhpotsdam/unfolding/utils/GeneralizationUtils.java
+++ src/de/fhpotsdam/unfolding/utils/GeneralizationUtils.java
@@ -5,48 +5,141 @@ import java.util.List;
 import java.util.Stack;
 
 import processing.core.PVector;
+import de.fhpotsdam.unfolding.geo.Location;
 
 /**
- * Generalizes polygons.
- * 
- * Based on Simplify.js by Vladimir Agafonkin.
- * 
- * Ported to Java and modified for Unfolding, Till Nagel.
- * 
- * 
- * 
- * Copyright (c) 2012, Vladimir Agafonkin. All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following
- * disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ * Provides utility functions to simplify points, paths, and polygons.
  */
 public class GeneralizationUtils {
 
-	// square distance between 2 points
-	public static float getSquareDistance(PVector p1, PVector p2) {
+	/**
+	 * Calculates the moving average of vertices. Can be used to smoothen paths when the vertices are geo-locations.
+	 * 
+	 * <p>
+	 * Opposite to {@link #simplify(List, float, boolean)} this keeps the same amount of vertices, so does not improve
+	 * performance, but creates smoother paths. You might want to combine both methods.
+	 * </p>
+	 * 
+	 * @param vertices
+	 *            The vertices to smoothen.
+	 * @param np
+	 *            The number of points to average over.
+	 * @return The smoothened vertices.
+	 */
+	public static List<PVector> computeMovingAverage(List<PVector> vertices, int np) {
+		// Convert to primitive arrays for both x and y
+		float[] xValues = new float[vertices.size()];
+		float[] yValues = new float[vertices.size()];
+		for (int i = 0; i < vertices.size(); i++) {
+			xValues[i] = vertices.get(i).x;
+			yValues[i] = vertices.get(i).y;
+		}
+
+		// Compute moving averages
+		float[] xAvgValues = computeMovingAverage(xValues, np);
+		float[] yAvgValues = computeMovingAverage(yValues, np);
+
+		// Combine averaged x and y back to vertices
+		List<PVector> averageVertices = new ArrayList<PVector>();
+		for (int i = 0; i < xAvgValues.length; i++) {
+			averageVertices.add(new PVector(xAvgValues[i], yAvgValues[i]));
+		}
+
+		return averageVertices;
+	}
+
+	/**
+	 * Calculates the np-point moving average for some values.
+	 * 
+	 * @param np
+	 *            number of points to average over.
+	 * @return The smoothened values.
+	 */
+	protected static float[] computeMovingAverage(float[] values, int np) {
+		float[] mm = new float[values.length - np];
+		for (int i = 0; i < mm.length; i++) {
+			float sum = 0;
+			for (int j = 0; j < np; j++) {
+				sum = sum + values[i + j];
+			}
+			mm[i] = sum / np;
+		}
+		return mm;
+	}
+
+	/**
+	 * Simplifies points by using Douglas-Peucker algorithm. Can be used to reduce complexity. This is zoom-dependent
+	 * when used for {@link Location}s due to the tolerance value.
+	 * 
+	 * <p>
+	 * Opposite to {@link #computeMovingAverage(List, int)} this reduces the amount of vertices, and thus improves
+	 * performance, but may result in still too complex paths. You might want to combine both methods.
+	 * </p>
+	 * 
+	 * 
+	 * <p>
+	 * Based on Simplify.js by Vladimir Agafonkin. Ported to Java and modified for Unfolding by Till Nagel.
+	 * 
+	 * <p>
+	 * Copyright (c) 2012, Vladimir Agafonkin. All rights reserved.
+	 * 
+	 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+	 * following conditions are met:
+	 * 
+	 * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+	 * following disclaimer.
+	 * 
+	 * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+	 * following disclaimer in the documentation and/or other materials provided with the distribution.
+	 * 
+	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+	 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+	 * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+	 * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	 * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+	 * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 * </p>
+	 * </p>
+	 * 
+	 * @param points
+	 *            A list of vertices.
+	 * @param tolerance
+	 *            The amount of simplification (in the same metric as the points).
+	 * @param highestQuality
+	 *            Whether to exclude distance-based preprocessing step which leads to highest quality simplification but
+	 *            runs ~10-20 times slower.
+	 * @return A list of simplified vertices.
+	 */
+	public static List<PVector> simplify(List<PVector> points, float tolerance, boolean highestQuality) {
+		float sqTolerance = tolerance * tolerance;
+
+		if (!highestQuality) {
+			points = simplifyRadialDistance(points, sqTolerance);
+		}
+
+		// int numPoints = points.size();
+		points = simplifyDouglasPeucker(points, sqTolerance);
+		// int numSimplifiedPoints = points.size();
+		// PApplet.println("Reduced " + numPoints + " to " + numSimplifiedPoints + " points.");
+
+		return points;
+	};
+
+	/**
+	 * Square distance between 2 points.
+	 */
+	protected static float getSquareDistance(PVector p1, PVector p2) {
 		float dx = p1.x - p2.x;
 		float dy = p1.y - p2.y;
 
 		return dx * dx + dy * dy;
 	}
 
-	// square distance from a point to a segment
-	public static float getSquareSegmentDistance(PVector p, PVector p1, PVector p2) {
+	/**
+	 * Square distance from a point to a segment.
+	 */
+	protected static float getSquareSegmentDistance(PVector p, PVector p1, PVector p2) {
 		float x = p1.x;
 		float y = p1.y;
 
@@ -75,8 +168,16 @@ public class GeneralizationUtils {
 		return dx * dx + dy * dy;
 	}
 
-	// distance-based simplification
-	public static List<PVector> simplifyRadialDistance(List<PVector> points, float sqTolerance) {
+	/**
+	 * Distance-based simplification.
+	 * 
+	 * @param points
+	 *            List of vertices.
+	 * @param sqTolerance
+	 *            The square tolerance value.
+	 * @return A list of simplified vertices.
+	 */
+	protected static List<PVector> simplifyRadialDistance(List<PVector> points, float sqTolerance) {
 
 		int i;
 		float len = points.size();
@@ -101,8 +202,16 @@ public class GeneralizationUtils {
 		return newPoints;
 	}
 
-	// simplification using optimized Douglas-Peucker algorithm with recursion elimination
-	public static List<PVector> simplifyDouglasPeucker(List<PVector> points, float sqTolerance) {
+	/**
+	 * Simplification using optimized Douglas-Peucker algorithm with recursion elimination.
+	 * 
+	 * @param points
+	 *            List of vertices.
+	 * @param sqTolerance
+	 *            The square tolerance value.
+	 * @return A list of simplified vertices.
+	 */
+	protected static List<PVector> simplifyDouglasPeucker(List<PVector> points, float sqTolerance) {
 
 		int len = points.size();
 
@@ -170,21 +279,5 @@ public class GeneralizationUtils {
 
 		return newPoints;
 	}
-
-	public static List<PVector> simplify(List<PVector> points, float tolerance, boolean highestQuality) {
-
-		float sqTolerance = tolerance * tolerance;
-
-		if (!highestQuality) {
-			points = simplifyRadialDistance(points, sqTolerance);
-		}
-
-		// int numPoints = points.size();
-		points = simplifyDouglasPeucker(points, sqTolerance);
-		// int numSimplifiedPoints = points.size();
-		// PApplet.println("Reduced " + numPoints + " to " + numSimplifiedPoints + " points.");
-
-		return points;
-	};
 
 }
