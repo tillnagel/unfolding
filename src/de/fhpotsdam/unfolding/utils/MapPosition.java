417eecacbc1b2e18bef28c29d1562b29ffb448c0
Till Nagel <github@tillnagel.com>
2013-07-15 16:55:49 +0200

    Added javadoc.

--- src/de/fhpotsdam/unfolding/utils/MapPosition.java
+++ src/de/fhpotsdam/unfolding/utils/MapPosition.java
@@ -2,12 +2,16 @@ package de.fhpotsdam.unfolding.utils;
 
 import processing.core.PVector;
 
+/**
+ * Class used to store positions on a specific map. Used internally by some markers. These are neither geo-locations,
+ * nor screen positions, but the position on the map object in screen coordinate system.
+ */
 @SuppressWarnings("serial")
-public class MapPosition extends PVector{
+public class MapPosition extends PVector {
 	public MapPosition() {
 	}
-	
-	public MapPosition(float []xy){
+
+	public MapPosition(float[] xy) {
 		x = xy[0];
 		y = xy[1];
 	}
