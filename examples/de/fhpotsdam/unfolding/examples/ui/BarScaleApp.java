ebf8ac2d6beb2d903177b8dd692536d2f775640e
tillnagel <github@tillnagel.com>
2013-12-10 22:11:44 +0100

    Auth test.

--- examples/de/fhpotsdam/unfolding/examples/ui/BarScaleApp.java
+++ examples/de/fhpotsdam/unfolding/examples/ui/BarScaleApp.java
@@ -2,12 +2,10 @@ package de.fhpotsdam.unfolding.examples.ui;
 
 import processing.core.PApplet;
 import processing.core.PFont;
-
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.ui.BarScaleUI;
 import de.fhpotsdam.unfolding.utils.MapUtils;
-
 
 /**
  * Simple map example with a styled bar scale (at the right top).
@@ -24,11 +22,11 @@ public class BarScaleApp extends PApplet {
 		map = new UnfoldingMap(this);
 		map.zoomAndPanTo(new Location(52.5f, 13.4f), 10);
 		MapUtils.createDefaultEventDispatcher(this, map);
-		
-		//add a bar scale to your map
+
+		// add a bar scale to your map
 		barScale = new BarScaleUI(this, map, 700, 20);
 
-		//optionally style your bar scale
+		// optionally style your bar scale
 		PFont myFont = createFont("Monospaced", 12);
 		barScale.setStyle(color(60, 120), 6, -2, myFont);
 	}
@@ -36,6 +34,7 @@ public class BarScaleApp extends PApplet {
 	public void draw() {
 		background(0);
 		map.draw();
+
 		barScale.draw();
 	}
 
