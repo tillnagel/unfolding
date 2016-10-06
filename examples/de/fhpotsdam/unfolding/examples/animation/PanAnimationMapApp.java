7446956d3dd06a6d2b28427e4ba069d2f697e75b
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-06 15:29:32 +0300

    improve

--- examples/de/fhpotsdam/unfolding/examples/animation/PanAnimationMapApp.java
+++ examples/de/fhpotsdam/unfolding/examples/animation/PanAnimationMapApp.java
@@ -9,43 +9,47 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /**
  * Pans smoothly between three locations, in an endless loop.
  * 
- * Press SPACE to switch tweening off (and on again).
+ * Press SPACE to switch twining off (and on again).
  * 
  * See {@link NaviButtonMapApp} for an interactive example on how to animate between two locations.
  */
 public class PanAnimationMapApp extends PApplet {
 
-	UnfoldingMap map;
-
-	Location[] locations = new Location[] { new Location(52.5, 13.4), new Location(53.6f, 10),
+	private static final int ZOOM_LEVEL = 8;
+	private final static Location[] LOCATIONS = new Location[] { 
+			new Location(52.5, 13.4), 
+			new Location(53.6f, 10),
 			new Location(51.34, 12.37) };
-	int currentLocation = 0;
-
+	private int currentLocation = 0;
+	private UnfoldingMap map;
+	
+	@Override
 	public void settings() {
 		size(800, 600, P2D);
 	}
 
+	@Override
 	public void setup() {
 		map = new UnfoldingMap(this);
 		map.setTweening(true);
-		map.zoomAndPanTo(8, locations[currentLocation]);
-
+		map.zoomAndPanTo(ZOOM_LEVEL, LOCATIONS[currentLocation]);
 		MapUtils.createDefaultEventDispatcher(this, map);
 	}
 
+	@Override
 	public void draw() {
 		background(0);
 		map.draw();
-
 		if (frameCount % 120 == 0) {
-			map.panTo(locations[currentLocation]);
+			map.panTo(LOCATIONS[currentLocation]);
 			currentLocation++;
-			if (currentLocation >= locations.length) {
+			if (currentLocation >= LOCATIONS.length) {
 				currentLocation = 0;
 			}
 		}
 	}
 
+	@Override
 	public void keyPressed() {
 		if (key == ' ') {
 			map.switchTweening();
@@ -53,7 +57,7 @@ public class PanAnimationMapApp extends PApplet {
 	}
 
 	public static void main(String[] args) {
-		PApplet.main(new String[] { "de.fhpotsdam.unfolding.examples.animation.PanAnimationMapApp" });
+		PApplet.main(new String[] { PanAnimationMapApp.class.getName() });
 	}
 
 }
