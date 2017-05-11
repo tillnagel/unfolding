3ef5609b0f03f05773ba1442fb76c22794c27c05
tillnagel <github@tillnagel.com>
2017-05-11 18:25:24 +0200

    Updated MapBox map provider.

--- src/de/fhpotsdam/unfolding/providers/MapBox.java
+++ src/de/fhpotsdam/unfolding/providers/MapBox.java
@@ -24,7 +24,6 @@ public class MapBox {
 		}
 
 		public String getPositiveZoomString(Coordinate coordinate) {
-			// Rows are numbered from bottom to top (opposite to OSM)
 			float gridSize = PApplet.pow(2, coordinate.zoom);
 
 			return (int) coordinate.zoom + "/" + (int) coordinate.column + "/" + (int) coordinate.row;
@@ -47,9 +46,31 @@ public class MapBox {
 
 	public static class LacquerProvider extends MapBoxProvider {
 		public String[] getTileUrls(Coordinate coordinate) {
-			String url = "http://c.tiles.mapbox.com/v3/mapbox.mapbox-lacquer/" + getPositiveZoomString(coordinate) + ".png";
+			String url = "http://c.tiles.mapbox.com/v3/mapbox.mapbox-lacquer/" + getPositiveZoomString(coordinate)
+					+ ".png";
 			return new String[] { url };
 		}
+	}
+
+	public static class CustomMapBoxProvider extends MapBoxProvider {
+		String urlTemplate;
+
+		public CustomMapBoxProvider(String urlTemplate) {
+			this.urlTemplate = urlTemplate;
+		}
+
+		public String[] getTileUrls(Coordinate coordinate) {
+			int zoom = (int) coordinate.zoom;
+			int col = (int) coordinate.column;
+			int row = (int) coordinate.row;
+
+			String url = urlTemplate.replace("{z}", "" + zoom);
+			url = url.replace("{x}", "" + col);
+			url = url.replace("{y}", "" + row);
+
+			return new String[] { url };
+		}
+
 	}
 
 	// For testing non-exported map styles from your local TileMill server.
@@ -61,22 +82,20 @@ public class MapBox {
 			return new String[] { url };
 		}
 	}
-	
+
 	/** Example for local TileMill usage, for testing purposes. For actual usage tiles should be exported. */
 	public static class PlainUSAProvider extends MapBoxProvider {
-		
+
 		// TODO Use new TileMill app
-		//http://localhost:20008/tile/border-bumps/{z}/{x}/{y}.png?updated=' + new Date().getTime()
-		
+		// http://localhost:20008/tile/border-bumps/{z}/{x}/{y}.png?updated=' + new Date().getTime()
+
 		public String[] getTileUrls(Coordinate coordinate) {
-			String url = "http://localhost:8889/1.0.0/plain-usa/"
-					+ getZoomString(coordinate) + ".png";
+			String url = "http://localhost:8889/1.0.0/plain-usa/" + getZoomString(coordinate) + ".png";
 			return new String[] { url };
 		}
 	}
-	
-	
-	// REMOVE 
+
+	// REMOVE
 	public static class BlankProvider extends MapBoxProvider {
 		public String[] getTileUrls(Coordinate coordinate) {
 			String url = "http://tillnagel.com/transparent255.png";
